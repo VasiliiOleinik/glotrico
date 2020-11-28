f8e4775d19d1b29502e9081a4b05447080fdb39d
Vasilii <goooneoooogle@gmail.com>
2020-11-28 18:57:02 +0200

    выпадающий список профиля

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,13 @@
-$(function() {
-	
+$(function () {
+  $('#profile-ico').on('click', function () {
+    $(this).siblings('.profile-dropdown').fadeIn();
+  });
+
+  $(document).mouseup(function (e) {
+    var div = $(".profile-dropdown");
+    if (!div.is(e.target)
+      && div.has(e.target).length === 0) {
+      div.fadeOut();
+    }
+  });
 });
